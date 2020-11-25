9f7b388dfba080e6b2b7d22273f069d816f933f3
Lander Brandt <landerbrandt@gmail.com>
2020-11-24 18:44:01 -0800

    First commit

---
+++ Move-WemFiles.ps1
@@ -0,0 +1,16 @@
+﻿function Move-WemFiles {
+    [CmdletBinding()]
+    param (
+        [string]$SfxDirectory,
+        [string]$ModDirectory
+    )
+
+    $files = Get-ChildItem -Path $SfxDirectory -Recurse -Include *.wem
+    foreach ($file in $files) {
+        $nameParts = $file.Name -split "_"
+        $newName = ($nameParts[0..($nameParts.Length-2)] -join "_") + ".wem"
+        $targetFile = Join-Path $ModDirectory $newName
+        Write-Host $targetFile
+        Copy-Item -Path $file.FullName -Destination $targetFile -Force
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# O7 Sound Mod for World of Warships
+
+## Commands
+
+Move-WemFiles -SfxDirectory "%USERPROFILE%\WwiseProjects\O7SoundPack\.cache\Windows\SFX" -ModDirectory "D:\SteamLibrary\steamapps\common\World of Warships\bin\3052606\res_mods\banks\mods\O7"
+
+
+```
+.\ffmpeg.exe -i '..\..\Bfk_Is_This_Good.mp4' -vn '..\..\O7 Sound pack\QuickCommands\Bfk_Is_This_Good.mp3'
+```
